474b73c6f0c5f0b5f2e979e17c4e04746d0c3750
Kota Hashihama <kotahashihama@gmail.com>
2020-04-11 19:16:01 +0900

    feat(cron): change rain forecast schedule
    every 10 mins from 8 a.m. to 11 p.m.

--- cron.js
+++ cron.js
@@ -1,6 +1,6 @@
 const cron = require('node-cron')
 const WeatherCheckerMain = require('./WeatherCheckerMain')
 
-cron.schedule('* */10 * * * *', () => {
+cron.schedule('00 00,10,20,30,40,50 00,01,08-23 * * *', () => {
   WeatherCheckerMain.check()
 })
