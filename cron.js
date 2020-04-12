73507a4f5b31766c78cc237e70620262279adaff
Kota Hashihama <kotahashihama@gmail.com>
2020-04-12 23:27:45 +0900

    fix: fix end time of rain forecast
    To stop rain forecast by 23:00, set cron's end hour to 22

--- cron.js
+++ cron.js
@@ -1,6 +1,6 @@
 const cron = require('node-cron')
 const WeatherCheckerMain = require('./WeatherCheckerMain')
 
-cron.schedule('00 00,10,20,30,40,50 00,01,08-23 * * *', () => {
+cron.schedule('00 00,10,20,30,40,50 00,01,08-22 * * *', () => {
   WeatherCheckerMain.check()
 })
